262d2c65d09dac00fcc8f722100e9a84e554cf71
dikshantpatodia <dikshantpatodia@gmail.com>
2020-10-30 00:17:22 +0530

    Add get path without extension, add comment

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const imageFormats = ['png', 'jpg', 'jpeg', 'webp', 'gif'];
 
 const IMG_PREFIX = 'image/upload';
+
+/**
+ * To be used in CDN transformation
+ */
 const THUMBNAIL_TRANSFORMATION = 'w_144';
 const PREVIEW_TRANSFORMATION = 'w_300';
 
 function getExtension(path) {
   const fullFileName = path.substring(path.lastIndexOf('/') + 1);
   return fullFileName.substring(fullFileName.lastIndexOf('.') + 1).toLowerCase();
+}
+
+function getPathWithoutExtension(path) {
+  let lastIndex = path.lastIndexOf('.');
+  if (lastIndex <= 0) {
+    lastIndex = path.length;
+  }
+  return path.substring(0, lastIndex);
 }
